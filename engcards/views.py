5474fb59a226b86a168a0409c6c3fb386074a549
nickolaq <theknifenew@yandex.ru>
2022-03-30 01:51:52 +0400

    Update algorithm,templates,css.

--- engcards/views.py
+++ engcards/views.py
@@ -4,49 +4,52 @@ from django.shortcuts import render, redirect
 from .forms import *
 from .models import Word
 import random
+import time
 from django.views.generic import DetailView, UpdateView, DeleteView
 
 # Create your views here.
 from requests import request
 
 def play(request):
-    start=0
-    error="no error"
+    error=""
     form = SimpleForm()
     template_name = 'engcards/play.html'
-    print(start)
     AllWords = list(Word.objects.all())
-    answers=''
     if request.method == 'POST':
+        counter = int(request.POST.get('answer').split(',')[2])
         answer = request.POST.get('answer').split(',')[1]
         word = request.POST.get('answer').split(',')[0]
         print(Word.objects.get(trans=answer), word)
         if word == str(Word.objects.get(trans=answer)):
             print('YES')
             WordRandom = random.choice(AllWords)
-            answers = [WordRandom.trans, random.choice(AllWords).trans, random.choice(AllWords).trans]
-            random.shuffle(answers)
-            # data = {'form': form, 'WordRandom': WordRandom, 'answers': answers, 'ERROR': error}
+            answers=get_answers(WordRandom,5)
+            counter+=1
         else:
             print('NO')
-            error = "Nepravilno"
             WordRandom=Word.objects.get(word=word)
-            answers = [Word.objects.get(word=word).trans, random.choice(AllWords).trans, random.choice(AllWords).trans]
-            random.shuffle(answers)
-        data = {'form': form, 'WordRandom': WordRandom, 'answers': answers, 'ERROR': error}
+            error = "{} не перевод слова {}".format(answer,WordRandom)
+            answers=get_answers(WordRandom,5)
+            counter=0
+        time.sleep(0.4)
+        data = {'form': form, 'WordRandom': WordRandom, 'answers': answers, 'ERROR': error,'counter':counter}
         return render(request, template_name, data)
     else:
+        counter = 0
         WordRandom = random.choice(AllWords)
-        answers = [WordRandom.trans, random.choice(AllWords).trans, random.choice(AllWords).trans]
-        random.shuffle(answers)
-        # form.reload()
-        data = {'form': form, 'WordRandom': WordRandom, 'answers': answers, 'ERROR': error}
+        answers=get_answers(WordRandom,5)
+        data = {'form': form, 'WordRandom': WordRandom, 'answers': answers, 'ERROR': error,'counter':counter}
         return render(request, template_name, data)
 
-# class WordPlay(DetailView):
-#     model = Word
-#     template_name = 'engcards/play.html'
-#     form_class = WordForm
+def get_answers(word,count:int):
+    answers=[]
+    i=0
+    while i< count:
+        answers.append(random.choice(list(Word.objects.all())).trans)
+        i+=1
+    answers.append(word.trans)
+    random.shuffle(answers)
+    return answers
 
 def add(request):
     error = ''
